b1499742b8e4aff36a539860e5d2b6920b067c70
LeeoSilva <leeosilva0909@gmail.com>
2019-10-09 21:00:43 -0300

    some refactoring

--- headers/manageUltrasonics.hpp
+++ headers/manageUltrasonics.hpp
@@ -1,23 +1,28 @@
 #pragma once
 #include "../headers/vehicle.hpp"
 
+#define PIN_ULTRA_LADO_DIREITO 6
+#define PIN_ULTRA_LADO_ESQUERDO 2
+#define PIN_ULTRA_LADO_FRONTAL 2
+
+#define FAILED_STATUS -1 // Return value of a function that failed his purpose.
+#define SUCCESS_STATUS 0 // Return value of a function that success in his purpose.
+
+
 class manageUltrasonics{
 protected: 
 	vehicleStates state;
-	const unsigned char ultra_pins[2] = {1, 2};
 	unsigned northSide = 0, // distance variables for every ultrasonic sensor
 			 westSide  = 0,
-			 eastSide  = 0,
-			 southSide = 0;
+			 eastSide  = 0;
 
 public:
 	manageUltrasonics();
-	unsigned getNorthSide() const noexcept;
-	unsigned getWestSide() const noexcept;
-	unsigned getEastSide() const noexcept;
-	unsigned getSouthSide() const noexcept;
-	void	 setRightSideVel() const noexcept;
-	void	 setLeftSideVel() const noexcept; 
+	unsigned getFrontalSide() const noexcept;
+	unsigned getRightSide() const noexcept;
+	unsigned getLeftSide() const noexcept;
+	int setRightSideVel() const noexcept;
+	int setLeftSideVel() const noexcept; 
 		
-	int convert() const noexcept;
+	float convert() const noexcept;
 };
--- src/manageUltrasonics.cpp
+++ src/manageUltrasonics.cpp
@@ -2,16 +2,21 @@
 
 manageUltrasonics::manageUltrasonics(){
 	// Initializing all I/O ports for the Ultrasonic sensors.
-	for(unsigned i = 0; i < 2; ++i)
-		pinMode(ultra_pins[i], OUTPUT);
+	pinMode(PIN_ULTRA_LADO_DIREITO, OUTPUT);
+	pinMode(PIN_ULTRA_LADO_ESQUERDO, OUTPUT);
+	pinMode(PIN_ULTRA_LADO_FRONTAL,  OUTPUT);
 }
-unsigned manageUltrasonics::getNorthSide();
-unsigned manageUltrasonics::getWestSide();
-unsigned manageUltrasonics::getEastSide();
-unsigned manageUltrasonics::getSouthSide();
 
-void	 manageUltrasonics::setRightSideVel() const noexcept;
-void	 manageUltrasonics::setLeftSideVel() const noexcept; 
-	
-int convert() const noexcept;
+
+unsigned manageUltrasonics::getFrontalSide();
+unsigned manageUltrasonics::getRightSide();
+unsigned manageUltrasonics::getLeftSide();
+
+
+int manageUltrasonics::setRightSideVel() const noexcept;
+int manageUltrasonics::setLeftSideVel() const noexcept; 
+		
+
+inline int manageUltrasonics::convert(const long& microseconds) { return microseconds / 74 / 2; } // convert microseconds into cm.
+
 
